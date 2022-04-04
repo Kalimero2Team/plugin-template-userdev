4929259f813ac4fb356fcf446087c0110cf96f36
byquanton <32410361+byquanton@users.noreply.github.com>
2022-04-04 21:11:43 +0200

    Initial Commit

---
+++ src/main/java/com/kalimero2/team/template/TemplatePlugin.java
@@ -0,0 +1,10 @@
+package com.kalimero2.team.template;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TemplatePlugin extends JavaPlugin {
+  @Override
+  public void onEnable() {
+
+  }
+}
